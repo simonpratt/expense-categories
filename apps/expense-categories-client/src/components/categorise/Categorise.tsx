5b55d9476a2d3202bfd6d3d4f1cf4805cd32e592
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2024-06-17 07:59:31 +0930

    aider: Added a panel to the left side of the `Categorise` component to display existing categories and an action to add a new one.
    # Aider chat conversation:
    
    USER: Add a panel to the left side of this component. I want the panel to list out the existing categories and contain an action at the bottom to add a new one; similar in look and feel to jira epics. Each category has a name and colour. Please use material ui for react
    
    ASSISTANT: To add a panel to the left side of the `Categorise` component that lists existing categories and contains an action to add a new one, we will:
    
    1. Import the necessary Material UI components.
    2. Fetch the categories data.
    3. Create a panel on the left side to display the categories.
    4. Add a button at the bottom of the panel to add a new category.
    
    Here is the updated code:
    
    apps/expense-categories-client/src/components/categorise/Categorise.tsx
    ```typescript
    <<<<<<< SEARCH
    import React from 'react';
    import { apiConnector } from '../../core/api.connector';
    import { Table } from '@dtdot/lego';
    
    const Categorise = () => {
      const { data: transactionSummaries, isLoading } = apiConnector.app.transactions.getSummary.useQuery();
      return (
        <div>
          <Table>
            {transactionSummaries?.map((tx) => (
              <Table.Row key={tx.id}>
                <Table.Cell>{tx.description}</Table.Cell>
                <Table.Cell>{tx.totalDebit}</Table.Cell>
                <Table.Cell>{tx.totalFrequency}</Table.Cell>
              </Table.Row>
            ))}
          </Table>
        </div>
      );
    };
    
    export default Categorise;
    =======
    import React from 'react';
    import { apiConnector } from '../../core/api.connector';
    import { Table } from '@dtdot/lego';
    import { Box, Button, List, ListItem, ListItemText, ListItemIcon, Typography } from '@mui/material';
    import { Add as AddIcon, Circle as CircleIcon } from '@mui/icons-material';
    
    const Categorise = () => {
      const { data: transactionSummaries, isLoading } = apiConnector.app.transactions.getSummary.useQuery();
      const { data: categories } = apiConnector.app.categories.getAll.useQuery();
    
      return (
        <Box display="flex">
          <Box width="250px" bgcolor="grey.100" p={2} mr={2}>
            <Typography variant="h6">Categories</Typography>
            <List>
              {categories?.map((category) => (
                <ListItem key={category.id}>
                  <ListItemIcon>
                    <CircleIcon style={{ color: category.color }} />
                  </ListItemIcon>
                  <ListItemText primary={category.name} />
                </ListItem>
              ))}
            </List>
            <Button variant="contained" color="primary" startIcon={<AddIcon />}>
              Add Category
            </Button>
          </Box>
          <Box flex="1">
            <Table>
              {transactionSummaries?.map((tx) => (
                <Table.Row key={tx.id}>
                  <Table.Cell>{tx.description}</Table.Cell>
                  <Table.Cell>{tx.totalDebit}</Table.Cell>
                  <Table.Cell>{tx.totalFrequency}</Table.Cell>
                </Table.Row>
              ))}
            </Table>
          </Box>
        </Box>
      );
    };
    
    export default Categorise;
    >>>>>>> REPLACE
    ```

--- apps/expense-categories-client/src/components/categorise/Categorise.tsx
+++ apps/expense-categories-client/src/components/categorise/Categorise.tsx
@@ -1,21 +1,43 @@
 import React from 'react';
 import { apiConnector } from '../../core/api.connector';
 import { Table } from '@dtdot/lego';
+import { Box, Button, List, ListItem, ListItemText, ListItemIcon, Typography } from '@mui/material';
+import { Add as AddIcon, Circle as CircleIcon } from '@mui/icons-material';
 
 const Categorise = () => {
   const { data: transactionSummaries, isLoading } = apiConnector.app.transactions.getSummary.useQuery();
+  const { data: categories } = apiConnector.app.categories.getAll.useQuery();
+
   return (
-    <div>
-      <Table>
-        {transactionSummaries?.map((tx) => (
-          <Table.Row key={tx.id}>
-            <Table.Cell>{tx.description}</Table.Cell>
-            <Table.Cell>{tx.totalDebit}</Table.Cell>
-            <Table.Cell>{tx.totalFrequency}</Table.Cell>
-          </Table.Row>
-        ))}
-      </Table>
-    </div>
+    <Box display="flex">
+      <Box width="250px" bgcolor="grey.100" p={2} mr={2}>
+        <Typography variant="h6">Categories</Typography>
+        <List>
+          {categories?.map((category) => (
+            <ListItem key={category.id}>
+              <ListItemIcon>
+                <CircleIcon style={{ color: category.color }} />
+              </ListItemIcon>
+              <ListItemText primary={category.name} />
+            </ListItem>
+          ))}
+        </List>
+        <Button variant="contained" color="primary" startIcon={<AddIcon />}>
+          Add Category
+        </Button>
+      </Box>
+      <Box flex="1">
+        <Table>
+          {transactionSummaries?.map((tx) => (
+            <Table.Row key={tx.id}>
+              <Table.Cell>{tx.description}</Table.Cell>
+              <Table.Cell>{tx.totalDebit}</Table.Cell>
+              <Table.Cell>{tx.totalFrequency}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table>
+      </Box>
+    </Box>
   );
 };
 
