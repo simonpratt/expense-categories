a754fb2f625f1a342131393e11ac86911f1b7841
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2024-06-19 07:14:28 +0930

    feat: using virtual table to speed up render

--- apps/expense-categories-client/src/components/categorise/Categorise.tsx
+++ apps/expense-categories-client/src/components/categorise/Categorise.tsx
@@ -1,7 +1,9 @@
+// my-table.ts
 import React, { useState } from 'react';
 import { apiConnector } from '../../core/api.connector';
-import { Spacer, Loader } from '@dtdot/lego';
+import { Spacer, Loader, Themes } from '@dtdot/lego';
 import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { TableVirtuoso, TableComponents } from 'react-virtuoso';
 import AddCategoryModal from '../modals/AddCategoryModal';
 import EditCategoryModal from '../modals/EditCategoryModal';
 import CategoryList from './CategoryList';
@@ -32,6 +34,53 @@ const Categorise = () => {
     return <Loader variant='page-loader' />;
   }
 
+  const columns = [
+    { width: 200, label: 'Description', dataKey: 'description' },
+    { width: 150, label: 'Total Debit', dataKey: 'totalDebit', numeric: true },
+    { width: 150, label: 'Total Frequency', dataKey: 'totalFrequency', numeric: true },
+    { width: 120, label: 'Action', dataKey: 'action' },
+  ];
+
+  const VirtuosoTableComponents: TableComponents<(typeof transactionSummaries)[number]> = {
+    Scroller: React.forwardRef<HTMLDivElement>(function Scroll(props, ref) {
+      return <TableContainer {...props} ref={ref} />;
+    }),
+    Table: (props) => <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />,
+    TableHead: TableHead as any,
+    TableRow: (props) => <TableRow {...props} />,
+    TableBody: React.forwardRef<HTMLTableSectionElement>(function TBody(props, ref) {
+      return <TableBody {...props} ref={ref} />;
+    }),
+  };
+
+  const fixedHeaderContent = () => (
+    <TableRow>
+      {columns.map((column) => (
+        <TableCell
+          key={column.dataKey}
+          variant='head'
+          align={column.numeric || false ? 'right' : 'left'}
+          style={{ width: column.width }}
+          sx={{ backgroundColor: Themes.dark.colours.background }}
+        >
+          {column.label}
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+
+  const rowContent = (_index: number, row: (typeof transactionSummaries)[number]) => (
+    <React.Fragment>
+      {columns.map((column) => (
+        <TableCell key={column.dataKey} align={column.numeric || false ? 'right' : 'left'}>
+          {column.dataKey === 'totalDebit'
+            ? new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(row[column.dataKey])
+            : row[column.dataKey as keyof typeof row]}
+        </TableCell>
+      ))}
+    </React.Fragment>
+  );
+
   return (
     <Box display='flex'>
       <CategoryList
@@ -40,39 +89,24 @@ const Categorise = () => {
         setSelectedCategory={setSelectedCategory}
         setAddModalOpen={setAddModalOpen}
       />
-      <Box flex='1' pr={2} pt={1}>
+      <Box flex='1' pr={2} pt={1} display='flex' flexDirection='column'>
         <CategoryHeader
           selectedCategoryName={selectedCategoryName}
           selectedCategory={selectedCategory}
           setEditModalOpen={setEditModalOpen}
         />
         <Spacer size='1x' />
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Description</TableCell>
-                <TableCell>Total Debit</TableCell>
-                <TableCell>Total Frequency</TableCell>
-                <TableCell>Action</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {transactionSummaries
-                ?.filter((tx) => selectedCategory === 'all' || tx.spendingCategoryId === selectedCategory)
-                .map((tx) => (
-                  <TableRow key={tx.id}>
-                    <TableCell>{tx.description}</TableCell>
-                    <TableCell>
-                      {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(tx.totalDebit)}
-                    </TableCell>
-                    <TableCell>{tx.totalFrequency}</TableCell>
-                    <TableCell>todo action goes here</TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <div style={{ width: '100%', height: 'calc(100vh - 140px)' }}>
+          <TableVirtuoso
+            data={transactionSummaries.filter(
+              (tx) => selectedCategory === 'all' || tx.spendingCategoryId === selectedCategory,
+            )}
+            components={VirtuosoTableComponents}
+            fixedHeaderContent={fixedHeaderContent}
+            itemContent={rowContent}
+            style={{ height: '100%' }}
+          />
+        </div>
       </Box>
       {isAddModalOpen && <AddCategoryModal handleClose={() => setAddModalOpen(false)} />}
       {isEditModalOpen && selectedCategoryObj && (
