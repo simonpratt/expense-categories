7bffd3d1f329d95f918720db72005dd67c584aae
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2024-06-23 08:19:08 +0930

    feat: add description and default categories

--- apps/expense-categories-api/src/services/categories.service.ts
+++ apps/expense-categories-api/src/services/categories.service.ts
@@ -1,6 +1,72 @@
 import { prisma } from '../core/prisma.client';
 
+const defaultCategories = [
+  {
+    name: 'Groceries',
+    description:
+      "Expenses for food and household items typically bought at supermarkets, grocery stores, and farmers' markets.",
+    colour: 'green',
+  },
+  {
+    name: 'Dining Out',
+    description: 'Costs associated with eating at restaurants, cafes, food trucks, or ordering takeout/delivery.',
+    colour: 'yellow',
+  },
+  {
+    name: 'Transportation',
+    description:
+      'Expenses related to getting around, including public transit, fuel, car maintenance, ride-sharing services, and parking fees.',
+    colour: 'blue',
+  },
+  {
+    name: 'Shopping (General Retail)',
+    description:
+      'Purchases of non-grocery items like clothing, electronics, books, and other general merchandise from department stores or online retailers.',
+    colour: 'purple',
+  },
+  {
+    name: 'Utilities',
+    description: 'Regular bills for essential services such as electricity, water, gas, internet, and phone.',
+    colour: 'red',
+  },
+  {
+    name: 'Entertainment',
+    description:
+      'Costs for leisure activities, including movies, concerts, streaming services, hobbies, and sports events.',
+    colour: 'yellow',
+  },
+  {
+    name: 'Health & Wellness',
+    description:
+      'Expenses related to maintaining health, including gym memberships, medical co-pays, prescriptions, and personal care items.',
+    colour: 'green',
+  },
+  {
+    name: 'Home Improvement',
+    description:
+      'Costs associated with maintaining or upgrading your living space, including repairs, renovations, and purchases from hardware stores.',
+    colour: 'blue',
+  },
+  {
+    name: 'Travel',
+    description:
+      'Expenses incurred during trips, including airfare, hotel stays, car rentals, and activities while on vacation.',
+    colour: 'yellow',
+  },
+  {
+    name: 'Education',
+    description: 'Costs related to learning, such as tuition fees, textbooks, online courses, and school supplies.',
+    colour: 'purple',
+  },
+];
+
 export const getCategories = async () => {
+  const categories = await prisma.spendingCategory.findMany();
+
+  if (!categories.length) {
+    await prisma.spendingCategory.createMany({ data: defaultCategories });
+  }
+
   return prisma.spendingCategory.findMany();
 };
 
